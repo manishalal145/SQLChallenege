444a335f7b69eace98885295e8bdbeb871ad8e54
Manisha Lal <manishalal1991@gmail.com>
2021-05-10 08:44:48 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -48,14 +48,14 @@ on d.dept_no = dd.dept_no where dd.dept_no in (
 
 --7.List all employees in the Sales and Development departments, including their employee number, last name,first name, and department name.
 SELECT  e.emp_no,
-        e.last_name,
-        e.first_name,
-        d.dept_name
+e.last_name,
+e.first_name,
+d.dept_name
 FROM employees AS e
-    INNER JOIN dept_emp AS de
-        ON (e.emp_no = de.emp_no)
-    INNER JOIN departments AS d
-        ON (de.dept_no = d.dept_no)
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no)
 WHERE d.dept_name IN ('Sales', 'Development')
 ORDER BY e.emp_no;
 
